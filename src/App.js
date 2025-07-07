5bdcdf35a993048d3091c4611008fca7c1b9a7f1
Suhani Khosla <suhanikhosla2@gmail.com>
2025-07-07 14:42:43 +0530

    made final changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
 import NotFound from "./NotFound";
@@ -8,30 +8,22 @@ import NotFound from "./NotFound";
 function App() {
   const title = "Welcome to the new blog";
   const likes = 50;
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/create">
-              <Create />
-            </Route>
-            <Route path="/blogs/:id">
-              <BlogDetails />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </Router>
   );
-
 }
 
 export default App;
--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = ({ blogs }) => {
         <div className="blog-preview" key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
             <h2>{ blog.title }</h2>
-            <p>Written by { blog.auth }</p>
+            <p>Written by { blog.author }</p>
           </Link>
         </div>
       ))}
